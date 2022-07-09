69c051b44c36dbdd679e1fe264c70525b3c60b1a
Rodolfo Gonzalez <42151698+rodogonzalez@users.noreply.github.com>
2022-07-09 13:41:55 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,10 @@
-[ToDo : Camare reader ]
+<form id="frm-validator" action="/validate" method="post">
+<input type="textfield" id="qr-text" value="" placeholder="Ingrese el Codigo AQUI" style="font-size:20px"></p>
+<p><input type="submit" value="Validar Cupon"></p>
+</form>
+<div id="qr-result">
+<h1 id="outputData" style="display:none"></h1>
+<p><img id="qr-found" src=""></p>
+<video id="preview"></video>
+<script type='text/javascript' src='https://rawgit.com/schmich/instascan-builds/master/instascan.min.js?ver=6.0' id='script-index-scan-js'></script>
+<script type='text/javascript' src='/js/qr-reader.js?ver=1.0' id='script-scanner-js'></script>
