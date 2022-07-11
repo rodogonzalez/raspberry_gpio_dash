1e7100aa6c258c837979b66064b9791b663efb8e
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2022-07-11 11:17:12 -0600

    improvements

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+/*
         $puertos_disponibles = ['18','17','20','26'];
         $estados = ['on','off','on','on','on'];
-        $total = 100; 
+        $total = 4; 
         for ($x = 0; $x < $total; $x++){
             $command_io = new \App\Models\ProcessQueue([
                 'command' => $estados[rand(0,1)] ,
@@ -26,13 +26,13 @@ class DatabaseSeeder extends Seeder
             ]);                 
             $command_io->save();
         }
+*/
 
-
-        $command_io = new \App\Models\ProcessQueue(['command' => 'off' ,'port' => $puertos_disponibles[ 0 ],'delay'=>  0 ,'status' => 'pending' ]);                 
+        $command_io = new \App\Models\ProcessQueue(['command' => 'on' ,'port' => $puertos_disponibles[ 0 ],'delay'=>  0 ,'status' => 'pending' ]);                 
         $command_io->save();
         $command_io = new \App\Models\ProcessQueue(['command' => 'off' ,'port' => $puertos_disponibles[ 1 ],'delay'=>  0 ,'status' => 'pending' ]);                 
         $command_io->save();
-        $command_io = new \App\Models\ProcessQueue(['command' => 'off' ,'port' => $puertos_disponibles[ 2 ],'delay'=>  0 ,'status' => 'pending' ]);                 
+        $command_io = new \App\Models\ProcessQueue(['command' => 'on' ,'port' => $puertos_disponibles[ 2 ],'delay'=>  0 ,'status' => 'pending' ]);                 
         $command_io->save();
         $command_io = new \App\Models\ProcessQueue(['command' => 'off' ,'port' => $puertos_disponibles[ 3 ],'delay'=>  0 ,'status' => 'pending' ]);                 
         $command_io->save();     
